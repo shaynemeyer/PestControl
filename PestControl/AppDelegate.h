4e943e2e3a253aa106c814a4fc276bf6f346f993
shaynemeyer <shayne.meyer@gmail.com>
2014-09-10 05:22:47 -0700

    Initial Commit

---
+++ PestControl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PestControl
+//
+//  Created by Shayne Meyer on 9/10/14.
+//  Copyright (c) 2014 Maynesoft LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
