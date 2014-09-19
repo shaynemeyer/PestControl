4afa8531c29f33f9a132dec69001e53b861636b9
shaynemeyer <shayne.meyer@gmail.com>
2014-09-19 06:51:13 -0700

    added physics bodies.

--- PestControl/TmxTileMapLayer.m
+++ PestControl/TmxTileMapLayer.m
@@ -7,6 +7,7 @@
 //
 
 #import "TmxTileMapLayer.h"
+#import "MyScene.h"
 
 @implementation TmxTileMapLayer{
     TMXLayer *_layer;
@@ -22,6 +23,7 @@
         _tmxTileSize = layer.mapTileSize;
         _tmxGridSize = layer.layerInfo.layerGridSize;
         _tmxLayerSize = CGSizeMake(layer.layerWidth, layer.layerHeight);
+        [self createNodesFromLayer:layer];
     }
     
     return self;
@@ -43,6 +45,41 @@
 -(CGSize)layerSize
 {
     return _tmxLayerSize;
+}
+
+#pragma mark
+#pragma mark - create methods
+
+-(void)createNodesFromLayer:(TMXLayer *)layer
+{
+    JSTileMap *map = layer.map;
+    // loop through all locations in the layer
+    for (int w = 0; w < self.gridSize.width; ++w) {
+        for (int h = 0; h < self.gridSize.height; ++h) {
+            CGPoint coord = CGPointMake(w, h);
+            // Find the Global Identifier (GID) with value of zero, no tile at that location, skip ahead to the next cell.
+            NSInteger tileGid = [layer.layerInfo tileGidAtCoord:coord];
+            if (!tileGid) {
+                continue;
+            }
+            // look for tiles with the wall property.
+            if ([map propertiesForGid:tileGid][@"wall"]) {
+                // attach a physics body to the tile.
+                SKSpriteNode *tile = [layer tileAtCoord:coord];
+                
+                tile.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:tile.size];
+                tile.physicsBody.categoryBitMask = PCWallCategory;
+                tile.physicsBody.dynamic = NO;
+                tile.physicsBody.friction = 0;
+            }
+        }
+    }
+}
+
+-(SKNode *)tileAtPoint:(CGPoint)point
+{
+    SKNode *tile = [super tileAtPoint:point];
+    return tile ? tile : [_layer tileAt:point];
 }
 
 @end
